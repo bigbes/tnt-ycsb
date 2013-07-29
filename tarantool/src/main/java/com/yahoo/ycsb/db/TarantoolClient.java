d7c6746c491c1d91e9bac034bed854ae3e4bee5e
Eugine Blikh <bigbes@gmail.com>
2013-07-29 18:25:42 +0400

    Merging tarantool and tnt-call. Errors in tnt-call now. Fix it. Added config file for tarantool

--- tarantool/src/main/java/com/yahoo/ycsb/db/TarantoolClient.java
+++ tarantool/src/main/java/com/yahoo/ycsb/db/TarantoolClient.java
@@ -1,48 +1,56 @@
 package com.yahoo.ycsb.db;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
+import java.util.List;
 import java.util.Vector;
+import java.util.HashMap;
+import java.util.Properties;
 import java.util.logging.Level;
 
-import org.tarantool.core.TarantoolConnection;
 import org.tarantool.core.Tuple;
-import org.tarantool.core.exception.CommunicationException;
+import org.tarantool.core.TarantoolConnection;
 import org.tarantool.core.exception.TarantoolException;
+import org.tarantool.core.exception.CommunicationException;
 import org.tarantool.core.impl.SocketChannelTarantoolConnection;
 
-import com.yahoo.ycsb.ByteIterator;
 import com.yahoo.ycsb.DB;
+import com.yahoo.ycsb.ByteIterator;
 import com.yahoo.ycsb.StringByteIterator;
 
 public class TarantoolClient extends DB{
 	TarantoolConnection connection;
 	
 	public static final String HOST_PROPERTY = "tnt.host";
-    public static final String PORT_PROPERTY = "tnt.port";
-	
-    private int space = 0;
-    
-    public void init() {
+	public static final String PORT_PROPERTY = "tnt.port";
+	public static final String CALL_PROPERTY = "tnt.call";
+
+	private boolean callValue;
+
+	private int space = 0;
+	public void init() throws CommunicationException {
 		Properties props = getProperties();
-        int port = 33013;
+		int port = 33013;
 		String address = props.getProperty(HOST_PROPERTY);
-		if (address == null){
+		if (address == null) {
 			address = "localhost";
 		}
 		String portString = props.getProperty(PORT_PROPERTY);
-		if (portString != null){
+		if (portString != null) {
 			port = Integer.parseInt(portString);
 		}
-        try{
-        	connection = new SocketChannelTarantoolConnection(address, port);
-        }
-        catch(CommunicationException e){
-        	e.printStackTrace();
-        }
+		String callString = props.getProperty(CALL_PROPERTY);
+		if (callString == null) {
+			this.callValue = false;
+		} else {
+			this.callValue = Boolean.parseBoolean(callString);
+		}
+		try {
+			connection = new SocketChannelTarantoolConnection(address, port);
+		} catch (CommunicationException e) {
+			e.printStackTrace();
+			throw e;
+		}
 	}
 	
 	public void cleanup(){
@@ -53,79 +61,118 @@ public class TarantoolClient extends DB{
 	public int insert(String table, String key,
 			HashMap<String, ByteIterator> values) {
 		int j = 0;
-		Tuple tuple = new Tuple(2*values.size() + 1);
-		tuple.setString(0, key, "UTF-8");
-		for (Map.Entry<String, ByteIterator> i: values.entrySet()){
-			tuple.setString(2*j + 1, i.getKey(), "UTF-8").setString(2*j + 2, 
-					i.getValue().toString(), "UTF-8");
-			j++;
-		}
-		try{
-			connection.insertOrReplace(space, tuple);
-		} catch (TarantoolException e){
+
+		Tuple tuple = new Tuple(2 * values.size() + (this.callValue ? 2 : 1));
+		try {
+			if (this.callValue) {
+				tuple.setLuaArgument(0, space);
+				tuple.setLuaString(1, key, "UTF-8");
+				for (Map.Entry<String, ByteIterator> i: values.entrySet()) {
+					tuple.setLuaString(2 * j + 2, i.getKey(), "UTF-8");
+					tuple.setLuaString(2 * j + 3, i.getValue().toString(), "UTF-8");
+					j++;
+				}
+				connection.call(space, "box.update", tuple);
+			}
+			else {
+				tuple.setString(0, key, "UTF-8");
+				for (Map.Entry<String, ByteIterator> i: values.entrySet()) {
+					tuple.setString(2 * j + 1, i.getKey(), "UTF-8");
+					tuple.setString(2 * j + 2, i.getValue().toString(), "UTF-8");
+					j++;
+				}
+				connection.insertOrReplace(space, tuple);
+			}
+		} catch (TarantoolException e) {
 			e.printStackTrace();
 			return 1;
 		}
 		return 0;
+	}
+
+	private HashMap<String, ByteIterator> tuple_convert_filter (Tuple response, 
+								    Set<String> fields) {
+		HashMap<String, ByteIterator> result = new HashMap<String, ByteIterator>();
+		if (response == null)
+			return result;
+		for (int i = 1; i < response.size(); i += 2) {
+			if (fields == null || fields.contains(response.getString(i, "UTF-8")))
+				result.put(response.getString(i, "UTF-8"), 
+					   new StringByteIterator(response.getString(i + 1, 
+										 "UTF-8")));
+		}
+		return result;
 	}
 
 	@Override
 	public int read(String table, String key, Set<String> fields,
-			HashMap<String, ByteIterator> result){
-		Tuple tup = new Tuple(1).setString(0, key, "UTF-8");
-		Tuple resp;
+			HashMap<String, ByteIterator> result) {
+		Tuple tuple = new Tuple(this.callValue ? 3 : 1);
+		Tuple resp = new Tuple(0);
 		try {
-			resp = connection.findOne(space, 0, 0, tup);
+			if (this.callValue) {
+				tuple.setLuaArgument(0, space);
+				tuple.setLuaArgument(1, 0);
+				tuple.setLuaString(2, key, "UTF-8");
+				resp = connection.call(space, "box.select", tuple).get(0);
+			} else {
+				tuple.setString(0, key, "UTF-8");
+				resp = connection.findOne(space, 0, 0, tuple);
+				if (resp == null) {
+					return 1;
+				}
+			}
 		} catch (TarantoolException e) {
+			e.printStackTrace();
 			return 1;
-		} 
-		if (resp == null)
+		} catch (IndexOutOfBoundsException e) {
+			e.printStackTrace();
 			return 1;
-		for (int i = 1; i < resp.size(); i += 2){
-			if (fields != null && fields.contains(resp.getString(i, "UTF-8")))
-				result.put(resp.getString(i, "UTF-8"), 
-						new StringByteIterator(resp.getString(i+1, "UTF-8")));
 		}
+		result = tuple_convert_filter(resp, fields);
 		return 0;
 	}
-
 	@Override
 	public int scan(String table, String startkey, int recordcount, Set<String> fields,
 			Vector<HashMap<String, ByteIterator>> result) {
-		Tuple args = new Tuple(4).setLuaArgument(0, space).setLuaArgument(1, 
-				0).setLuaArgument(2, recordcount).setLuaString(3, 
-						startkey,"UTF-8");
+		Tuple tuple = new Tuple(4);
+		tuple.setLuaArgument(0, space);
+		tuple.setLuaArgument(1, 0);
+		tuple.setLuaArgument(2, recordcount);
+		tuple.setLuaString(3, startkey, "UTF-8");
 		List<Tuple> response;
 		try {
-			response = connection.call(space, "box.select_range", args);
-		} catch (TarantoolException e){
+			response = connection.call(space, "box.select_range", tuple);
+		} catch (TarantoolException e) {
 			e.printStackTrace();
 			return 1;
 		}
-		HashMap<String, String> temp = new HashMap<String, String>();
-		for (Tuple i: response){
-			for (int j = 1; j < i.size(); j += 2){
-				if (fields != null && fields.contains(i.getString(j, "UTF-8")))
-					temp.put(i.getString(j, "UTF-8"), 
-							i.getString(j+1, "UTF-8"));
-			}
-			if (temp.size() != 0)
-				result.add((HashMap<String, ByteIterator>) temp.clone());
-			temp.clear();
+		for (Tuple i: response) {
+			HashMap<String, ByteIterator> temp = tuple_convert_filter(i, fields);
+			if (temp.isEmpty())
+				continue;
+			result.add((HashMap<String, ByteIterator>) temp.clone());
 		}
 		return 0;
 	}
-
 	@Override
 	public int delete(String table, String key) {
-		try{
-			connection.delete(space, new Tuple(1).setString(0, key, "UTF-8"));
-		} catch (TarantoolException e){
+		Tuple tuple = new Tuple(this.callValue ? 2 : 1);
+		try {
+			if (this.callValue) {
+				tuple.setLuaArgument(0, space);
+				tuple.setLuaString(1, key, "UTF-8");
+				connection.call(space, "box.delete", tuple);
+			} else {
+				tuple.setString(0, key, "UTF-8");
+				connection.delete(space, tuple);
+			}
+		} catch (TarantoolException e) {
 			e.printStackTrace();
 			return 1;
 		}
 		return 0;
-	}
+	} 
 	@Override
 	public int update(String table, String key,
 			HashMap<String, ByteIterator> values) {
